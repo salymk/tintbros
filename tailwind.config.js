0e2c47134690ec8fd8af97248154b2e97255c344
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-11 20:40:13 -0700

    Added tailwindcss through gatsby recipes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-// See https://tailwindcss.com/docs/configuration for details
 module.exports = {
   purge: ['./src/**/*.js'],
-  // https://github.com/tailwindlabs/tailwindcss-forms
-  plugins: [require('@tailwindcss/forms')],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
 };
