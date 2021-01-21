2c9e60dfcb3c513e8fb1420659f41d03f1e1eb9f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-21 16:50:26 -0700

    Added id for scroll effect

--- src/components/contact.js
+++ src/components/contact.js
@@ -91,7 +91,7 @@ const Contact = () => {
               </dl>
             </div>
           </div>
-          <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+          <div id="Schedule" className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
             <div className="max-w-lg mx-auto lg:max-w-none">
               <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
               <div>
