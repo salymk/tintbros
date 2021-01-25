12ef215ae0c4e8a7e9a2c7749080b52c2a84b53d
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-24 20:36:15 -0700

    Qeured seo data into layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,97 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Footer from './footer';
+import { useStaticQuery, graphql } from 'gatsby';
+import { SEOContext } from 'gatsby-plugin-wpgraphql-seo';
 
+import Footer from './footer';
 import Header from './header';
 
 function Layout({ children }) {
-  return (
-    <div className="flex flex-col min-h-screen font-sans text-gray-900">
-      <Header />
+  const {
+    wp: { seo },
+  } = useStaticQuery(graphql`
+    query SiteInfoQuery {
+      wp {
+        seo {
+          contentTypes {
+            post {
+              title
+              schemaType
+              metaRobotsNoindex
+              metaDesc
+            }
+            page {
+              metaDesc
+              metaRobotsNoindex
+              schemaType
+              title
+            }
+          }
+          webmaster {
+            googleVerify
+            yandexVerify
+            msVerify
+            baiduVerify
+          }
+          schema {
+            companyName
+            personName
+            companyOrPerson
+            wordpressSiteName
+            siteUrl
+            siteName
+            inLanguage
+            logo {
+              sourceUrl
+              mediaItemUrl
+              altText
+            }
+          }
+          social {
+            facebook {
+              url
+              defaultImage {
+                sourceUrl
+                mediaItemUrl
+              }
+            }
+            instagram {
+              url
+            }
+            linkedIn {
+              url
+            }
+            mySpace {
+              url
+            }
+            pinterest {
+              url
+              metaTag
+            }
+            twitter {
+              username
+            }
+            wikipedia {
+              url
+            }
+            youTube {
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
 
-      <main>{children}</main>
-      <Footer />
-    </div>
+  return (
+    <SEOContext.Provider value={{ global: seo }}>
+      <div className="flex flex-col min-h-screen font-sans text-gray-900">
+        <Header />
+
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </SEOContext.Provider>
   );
 }
 
