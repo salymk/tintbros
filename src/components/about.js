6fafe7c0e6b19593c2b5e2052c264bb867840cf4
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-24 20:32:31 -0700

    Changed text size

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ const About = () => (
               className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
             />
             <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-              <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+              <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-12 lg:aspect-none lg:h-full">
                 <img
                   className="object-cover lg:h-full lg:w-full"
                   src="https://images.unsplash.com/photo-1548984120-2d55e48b40bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80"
@@ -24,7 +24,7 @@ const About = () => (
             <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
               <blockquote>
                 <div>
-                  <p className="mt-6 text-2xl text-white">
+                  <p className="mt-6 text-lg md:text-2xl text-white">
                     Meet Idris and his little brother Umar, the ones bringing
                     the most tinted tinting eva. Sed urna nulla vitae laoreet
                     augue. Amet feugiat est integer dolor auctor adipiscing nunc
