15b6ca09df3cfb1fb90ec575879a2f5aa1312c6f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-30 08:15:03 -0700

    Added href for phone number and quered  custome field for number

--- src/components/header.js
+++ src/components/header.js
@@ -41,6 +41,7 @@ const Header = () => {
               item
             }
             phoneCta {
+              number
               title
               icon {
                 altText
@@ -179,31 +180,21 @@ const Header = () => {
             </div>
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <button
-                  type="button"
-                  className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm sm:text-md font-extrabold rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-800 focus:ring-red-500"
-                >
-                  <img
-                    className="h-5 sm:h-6 fill-current mr-2"
-                    src={phoneCta.icon.sourceUrl}
-                    alt={phoneCta.icon.altText}
-                  />
-                  {/* <svg
-                    className="h-5 sm:h-6 fill-current mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                {/* TODO: Add custom field for number */}
+                <a href={`tel:${phoneCta.number}`}>
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm sm:text-md font-extrabold rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-800 focus:ring-red-500"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                    <img
+                      className="h-5 sm:h-6 fill-current mr-2"
+                      src={phoneCta.icon.sourceUrl}
+                      alt={phoneCta.icon.altText}
                     />
-                  </svg> */}
 
-                  <span className="uppercase">{phoneCta.title}</span>
-                </button>
+                    <span className="uppercase">{phoneCta.title}</span>
+                  </button>
+                </a>
               </div>
             </div>
           </div>
