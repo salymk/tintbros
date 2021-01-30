c70dde2ca29961ef99adc2dd0be319fc045e7534
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-01-30 13:30:09 -0700

    Changed gradient colors

--- src/components/call-to-action.js
+++ src/components/call-to-action.js
@@ -44,7 +44,7 @@ const CallToAction = () => {
         <div className="flex-1" />
       </div>
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="bg-gradient-to-r from-red-400 to-black rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+        <div className="bg-gradient-to-r from-red-400 to-gray-900 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
           <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
             <div className="lg:self-center">
               <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
